e89c51968b3e9071c7a0d96861b403522fc8f210
Justin Jay <justinrjay@gmail.com>
2017-10-23 14:32:23 -0500

    Add try catch to clean up failed images

--- src/color-thief.js
+++ src/color-thief.js
@@ -600,43 +600,51 @@ module.exports = (function () {
      */
     ColorThief.prototype.getPalette = function(sourceImage, colorCount, quality) {
 
-        if (typeof colorCount === 'undefined' || colorCount < 2 || colorCount > 256) {
+        if (typeof colorCount === 'undefined') {
             colorCount = 10;
         }
-        if (typeof quality === 'undefined' || quality < 1) {
+        if (typeof quality === 'undefined') {
             quality = 10;
         }
 
-        // Create custom CanvasImage object
-        var image      = new CanvasImage(sourceImage);
-        var imageData  = image.getImageData();
-        var pixels     = imageData.data;
-        var pixelCount = image.getPixelCount();
+        var image = null;
+        var palette = null;
+        try {
+            // Create custom CanvasImage object
+            image = new CanvasImage(sourceImage);
+            image.draw();
 
-        // Store the RGB values in an array format suitable for quantize function
-        var pixelArray = [];
-        for (var i = 0, offset, r, g, b, a; i < pixelCount; i = i + quality) {
-            offset = i * 4;
-            r = pixels[offset + 0];
-            g = pixels[offset + 1];
-            b = pixels[offset + 2];
-            a = pixels[offset + 3];
-            // If pixel is mostly opaque and not white
-            if (a >= 125) {
-                if (!(r > 250 && g > 250 && b > 250)) {
-                    pixelArray.push([r, g, b]);
+            var imageData = image.getImageData();
+            var pixels = imageData.data;
+            var pixelCount = image.getPixelCount();
+
+            // Store the RGB values in an array format suitable for quantize function
+            var pixelArray = [];
+            for (var i = 0, offset, r, g, b, a; i < pixelCount; i = i + quality) {
+                offset = i * 4;
+                r = pixels[offset + 0];
+                g = pixels[offset + 1];
+                b = pixels[offset + 2];
+                a = pixels[offset + 3];
+                // If pixel is mostly opaque and not white
+                if (a >= 125) {
+                    if (!(r > 250 && g > 250 && b > 250)) {
+                        pixelArray.push([r, g, b]);
+                    }
                 }
             }
+
+            // Send array to quantize function which clusters values
+            // using median cut algorithm
+            var cmap = MMCQ.quantize(pixelArray, colorCount);
+            palette = cmap.palette();
+
+        } finally {
+            // Clean up
+            if (image != null) {
+                image.removeCanvas();
+            }
         }
-
-        // Send array to quantize function which clusters values
-        // using median cut algorithm
-        var cmap    = MMCQ.quantize(pixelArray, colorCount);
-        var palette = cmap ? cmap.palette() : null;
-
-        // Clean up
-        image.removeCanvas();
-
         return palette;
     };
 
